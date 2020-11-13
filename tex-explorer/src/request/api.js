55d435a66aa9e6093e2dfb9f881e923018a837c2
17771857516@163.com <17771857516@163.com>
2020-11-13 18:07:11 +0800

    Signed-off-by: 17771857516@163.com <17771857516@163.com>

--- tex-explorer/src/request/api.js
+++ tex-explorer/src/request/api.js
@@ -1,6 +1,6 @@
 import { get, post } from './http'
 
-export const apiIndexInfo = p => get('admin/blockBrowser/indexInfo', p);  //首页数据
-export const apiTransaction = p => get('admin/blockBrowser/transaction', p);  //首页转账数据
-export const apiStockInfo = p => get('admin/blockBrowser/selectUserChargesByAddress', p);  //区块浏览器账号信息
-export const apiMoneyDetail = p => get('admin/blockBrowser/selectUserChargeByTranId', p);  //区块浏览器交易详情、转账详情
+export const apiIndexInfo = p => get('app/blockBrowser/indexInfo', p);  //首页数据
+export const apiTransaction = p => get('app/blockBrowser/transaction', p);  //首页转账数据
+export const apiStockInfo = p => get('app/blockBrowser/selectUserChargesByAddress', p);  //区块浏览器账号信息
+export const apiMoneyDetail = p => get('app/blockBrowser/selectUserChargeByTranId', p);  //区块浏览器交易详情、转账详情
